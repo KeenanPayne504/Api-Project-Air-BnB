b080b901a1c2cfc11d11e85f8716f7b6480334bd
Keenan Payne <keenanpayne504@gmail.com>
2022-12-18 20:18:17 -0600

    added seeds again

--- backend/db/seeders/20220927180432-spotimage.js
+++ backend/db/seeders/20220927180432-spotimage.js
@@ -30,6 +30,26 @@ module.exports = {
           url: "https://img.freepik.com/premium-vector/suburban-twostorey-house-exterior-with-balcony_8071-12316.jpg?w=2000",
           preview: true,
         },
+        {
+          spotId: 5,
+          url: "https://media.istockphoto.com/id/157193895/photo/beautiful-swimming-pool-at-an-estate-home.jpg?s=612x612&w=0&k=20&c=CdpPhGnQOxxLJnt4Q-KevD05kM3G-giGD5iLLatEQDM=",
+          preview: true,
+        },
+        {
+          spotId: 6,
+          url: "https://images.pexels.com/photos/186077/pexels-photo-186077.jpeg?cs=srgb&dl=pexels-binyamin-mellish-186077.jpg&fm=jpg",
+          preview: true,
+        },
+        {
+          spotId: 7,
+          url: "https://images.unsplash.com/photo-1600596542815-ffad4c1539a9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWFuc2lvbnxlbnwwfHwwfHw%3D&w=1000&q=80",
+          preview: true,
+        },
+        {
+          spotId: 8,
+          url: "https://media.istockphoto.com/id/1081470818/photo/summer-in-the-park-trees-alley.jpg?s=612x612&w=0&k=20&c=aI5zza635w3rakEOk4a2E5e66riGKM0NohNMu3LKrkg=",
+          preview: true,
+        },
       ],
       {}
     );
